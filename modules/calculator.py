2bf96c535961ee6fe8ab46ea09f52ae8ffec344f
DMadanbek <84191060+DMadanbek@users.noreply.github.com>
2021-06-16 10:22:22 +0300

    Update calculator.py

--- modules/calculator.py
+++ modules/calculator.py
@@ -1,8 +1,10 @@
-import plus
-import div
-import mul
-import substraction
+from modules import plus
+from modules import div
+from modules import mul
+from modules import substraction
 
 #print(plus.add.addition(1,2))
 #print(div.Div.div(4,2))
 #print(substraction.substract.sub(4,3))
+
+
