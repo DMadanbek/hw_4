57103bf8c38cfb3892ca5a513a2a262e27a4d2f0
DMadanbek <84191060+DMadanbek@users.noreply.github.com>
2021-06-16 16:18:13 +0300

    Update plus.py

--- modules/plus.py
+++ modules/plus.py
@@ -1,5 +1,10 @@
 class add:
-    def addition(val1,val2):
-        val1 = val1
-        val2 = val2
-        return int(val1 + val2)
+    def addition(*val1,**val2):
+        sum=0
+        for i in val1:
+            sum=sum+i
+        for n in val2.values():
+            sum=sum+n
+
+        return sum
+
